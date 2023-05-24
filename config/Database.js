adb1bb04239a47c1b15ca12b223f2467ec6a8f04
DandanIsmail42 <dandanismail42@gmail.com>
2023-05-24 18:06:57 +0700

    modif db

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('tht_db','root','root',{
-    host: 'localhost',
+const db = new Sequelize('freedb_tht_crud','freedb_dandan','wXq3P!f9fZgjWnR',{
+    host: 'sql.freedb.tech',
     dialect: "mysql"
 });
 
